7c8d94c7938585795e13ebafcd8374c783f329a4
LakshmireddyP <lakshmireddyperam2002@gmail.com>
2023-11-06 20:38:34 +0530

    EDA & model training

--- src/exception.py
+++ src/exception.py
@@ -11,10 +11,10 @@ def error_message_details(error,error_detail:sys):
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
-        self.error_messaga=error_message_details(error_message,error_detail=error_detail)
+        self.error_messags=error_message_details(error_message,error_detail=error_detail)
 
     def __str__(self) -> str:
-        return self.error_messaga
+        return self.error_messags
     
 if __name__=="__main__":
     try:
