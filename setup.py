586490e32898a0819e8802df7b027dd3fe4ec69c
LakshmireddyP <lakshmireddyperam2002@gmail.com>
2023-11-06 13:38:15 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+Hyphen_E_dot="-e ."
+def get_requirements(file_path:str)->List[str]:
+    '''This function will return the list of requirements'''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+    if Hyphen_E_dot in requirements:
+        requirements.remove(Hyphen_E_dot)
+    return requirements
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Lakshmireddy',
+author_email='lakshmireddyperam2002@gmail.com',
+install_requires=get_requirements('requirements.txt'),
+packages=find_packages()
+)    
